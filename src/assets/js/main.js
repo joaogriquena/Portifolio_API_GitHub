cef4f1a562ee76053ac9216b024720393c0b53e4
Joao Guilherme <jgriquena@gmail.com>
2023-03-10 22:19:53 -0300

    links portfolio 2.0

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -43,7 +43,7 @@ function updateLanguages(profileData) {
 function updatePortfolio(profileData) {
     const portfolio = document.getElementById('profile.portfolio')
     portfolio.innerHTML = profileData.portfolio.map(project => {
-        `
+        return `
         <li>
             <h3 ${project.github ? 'class="github"' : ''}>${project.name}</h3>
             <a href="${project.url}" target="_blank">${project.url}</a>
