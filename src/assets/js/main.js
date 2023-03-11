017a0db124aaf9d4b5bbad5b51b85e9eec252021
Joao Guilherme <jgriquena@gmail.com>
2023-03-10 22:19:36 -0300

    links portfolio

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -40,6 +40,18 @@ function updateLanguages(profileData) {
     languages.innerHTML = profileData.languages.map(language => `<li>${language}</li>`).join('')
 }
 
+function updatePortfolio(profileData) {
+    const portfolio = document.getElementById('profile.portfolio')
+    portfolio.innerHTML = profileData.portfolio.map(project => {
+        `
+        <li>
+            <h3 ${project.github ? 'class="github"' : ''}>${project.name}</h3>
+            <a href="${project.url}" target="_blank">${project.url}</a>
+        </li>
+        `
+    }).join('')
+}
+
 (async () => {
 
     const profileData = await fetchProfileData()
@@ -47,4 +59,5 @@ function updateLanguages(profileData) {
     updateSoftSkills(profileData)
     updateHardSkills(profileData)
     updateLanguages(profileData)
+    updatePortfolio(profileData)
 })()
