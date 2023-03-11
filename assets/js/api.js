282bf54846bcdc9d4b18eda4e2d8ebb7e4e80d33
Joao Guilherme <jgriquena@gmail.com>
2023-03-11 00:33:12 -0300

    trocando a url do json

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,6 @@
 
 async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/joaogriquena/Portifolio_API_GitHub/master/src/data/profile.json'
+    const url = 'https://raw.githubusercontent.com/joaogriquena/Portifolio_API_GitHub/master/data/profile.json'
     const response = await fetch(url)
     const profileData = await response.json()
     return profileData
